dcbff7c14278fea6d8818d496056dc469ed99e10
bailin <Thisisbailin@icloud.com>
2025-05-07 16:08:18 +0800

    Initial Commit

---
+++ SpurTests/SpurTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpurTests.swift
+//  SpurTests
+//
+//  Created by Joe on 2025/5/7.
+//
+
+import Testing
+@testable import Spur
+
+struct SpurTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
